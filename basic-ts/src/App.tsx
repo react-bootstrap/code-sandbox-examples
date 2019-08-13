bfc8c5e9dc817444269ab29cf8daaf278d9c85a1
Max Schmitt <max@schmitt.mx>
2019-08-13 13:47:19 +0200

    feat: added more showcases

--- basic-ts/src/App.tsx
+++ basic-ts/src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
-import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
+import ButtonsShowcase from './showcases/Buttons'
+import ToastsShowcase from './showcases/Toasts'
 
 const App: React.FC = () => {
   return (
-    <div className="p-1">
-      <Button variant="primary" className="mr-1">Primary</Button>
-      <Button variant="secondary" className="mr-1">Secondary</Button>
-      <Button variant="success" className="mr-1">Success</Button>
-      <Button variant="warning" className="mr-1">Warning</Button>
-      <Button variant="danger" className="mr-1">Danger</Button>
-      <Button variant="info" className="mr-1">Info</Button>
-      <Button variant="light" className="mr-1">Light</Button>
-      <Button variant="dark" className="mr-1">Dark</Button>
-      <Button variant="link" className="mr-1">Link</Button>
-    </div>
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">Welcome To React-Bootstrap Typescript Example</h1>
+      </Jumbotron>
+      <h2>Buttons</h2>
+      <ButtonsShowcase/>
+      <h2>Toasts</h2>
+      <ToastsShowcase/>
+    </Container>
   );
 }
 
