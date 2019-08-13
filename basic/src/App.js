f69d1c017c75242f7f064ad7b2bff3083da6a3bc
Max Schmitt <max@schmitt.mx>
2019-08-13 13:57:45 +0200

    feat: cleanup

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,34 +1,40 @@
 import React, { useState } from "react";
+
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Toast from "react-bootstrap/Toast";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
-// Importing the Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 const ExampleToast = ({ children }) => {
   const [show, toggleShow] = useState(true);
 
   return (
-    <Toast show={show} onClose={() => toggleShow(!show)}>
-      <Toast.Header>
-        <strong className="mr-auto">React-Bootstrap</strong>
-      </Toast.Header>
-      <Toast.Body>{children}</Toast.Body>
-    </Toast>
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
   );
 };
 
 const App = () => (
-  <Jumbotron fluid>
-    <h1 className="header">Welcome To React-Bootstrap</h1>
-    <ExampleToast className="toast">
-      We now have Toasts
-      <span role="img" aria-label="tada">
-        🎉
-      </span>
-    </ExampleToast>
-  </Jumbotron>
+  <Container className="p-3">
+    <Jumbotron>
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <ExampleToast className="toast">
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+    </Jumbotron>
+  </Container>
 );
 
 export default App;
