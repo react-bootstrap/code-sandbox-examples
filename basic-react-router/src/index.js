a3440d97992de8849c389ed470d265eda13783e0
Max Schmitt <max@schmitt.mx>
2020-01-07 16:25:53 +0100

    feat: added example for react-router-bootstrap fix: https://github.com/react-bootstrap/react-router-bootstrap/issues/254

---
+++ basic-react-router/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Importing the Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(<App />, document.getElementById('root'));
