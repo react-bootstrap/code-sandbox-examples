a5c94aada892a1a4baf0a0228861238a2c919d8e
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2021-10-22 17:24:10 -0700

    Add bootstrap v5 parcel examples and fix compiler error due to deprecated jumbotron (#245)

---
+++ parcel-v5/src/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <React.Fragment>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </React.Fragment>
+  );
+};
+
+const App = () => (
+  <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <ExampleToast>
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+    </Container>
+  </Container>
+);
+
+var mountNode = document.getElementById('root');
+ReactDOM.render(<App />, mountNode);
