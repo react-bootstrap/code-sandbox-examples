82b69dffffbfe48b5430b5ef2150e2ab9ae4eef1
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2021-10-22 17:23:44 -0700

    Add bootstrap v5 basic-cdn examples and fix compiler error due to deprecated jumbotron (#244)

---
+++ basic-cdn-v5/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+
+import './App.css';
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <Toast show={show} onClose={() => toggleShow(!show)}>
+      <Toast.Header>
+        <strong className="mr-auto">React-Bootstrap</strong>
+      </Toast.Header>
+      <Toast.Body>{children}</Toast.Body>
+    </Toast>
+  );
+};
+
+const App = () => (
+  <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <ExampleToast>
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+    </Container>
+  </Container>
+);
+
+export default App;
